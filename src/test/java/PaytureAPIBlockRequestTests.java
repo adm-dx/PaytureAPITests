187c10e1200f42c66315f6cc9643db964f85939d
Roman Hohlov <adm.dx@outlook.com>
2020-11-19 01:22:45 +0300

    Some tests added Added annotation Description All values moved to final vars Added Encode method

--- src/test/java/PaytureAPIBlockRequestTests.java
+++ src/test/java/PaytureAPIBlockRequestTests.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Description;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
@@ -13,15 +14,24 @@ public class PaytureAPIBlockRequestTests {
     final String KEY_VALUE = "Merchant";
     final String AMOUNT_NAME = "Amount";
     final String AMOUNT_VALUE = "12345";
-    final String ORDERID_NAME = "OrderId";
-    final String PAYINFO_NAME = "PayInfo";
+    final String ORDER_ID_NAME = "OrderId";
+    final String PAY_INFO_NAME = "PayInfo";
+
+    final String ERROR_CODE_ACCESS_DENIED = "ErrCode=\"ACCESS_DENIED\"";
+    final String ERROR_CODE_DUPLICATE_ORDER_ID = "ErrCode=\"DUPLICATE_ORDER_ID\"";
+    final String ERROR_CODE_WRONG_PARAMS = "ErrCode=\"WRONG_PARAMS\"";
+    final String ERROR_CODE_AMOUNT_ERROR = "ErrCode=\"AMOUNT_ERROR\"";
+    final String ERROR_CODE_WRONG_PAY_INFO = "ErrCode=\"WRONG_PAY_INFO\"";
+
     String orderID = orderIDBuilder();
+    String duplicateOrderId = "60f02253-bea1-2563-d432-961f0ace9c943";
+
     String payInfoValue = encodeValue("PAN=4111111111111112;EMonth=12;EYear=22;CardHolder=Roman Miller;SecureCode=123;" +
             "OrderId=" + orderID + ";Amount=" + AMOUNT_VALUE);
 
-    public static String orderIDBuilder(){
+    public static String orderIDBuilder() {
         //60f02253-bea1-2563-d432-961f0ace9c943
-        return "60f0" + (int)(Math.random() * 10000) + "-bea1-" + (int)(Math.random() * 10000) + "-d432-961f0ace9c943";
+        return "60f0" + (int) (Math.random() * 10000) + "-bea1-" + (int) (Math.random() * 10000) + "-d432-961f0ace9c943";
     }
 
     private static String encodeValue(String value) {
@@ -33,15 +43,16 @@ public class PaytureAPIBlockRequestTests {
     }
 
     @Test
+    @Description("Отправить GET запрос с корректно заполненными обязательными полями (Key, OrderID, Amount, PayInfo)")
     public void test_sendCorrectRequest() {
         given().
                 queryParam(KEY_NAME, KEY_VALUE).
                 queryParam(AMOUNT_NAME, AMOUNT_VALUE).
-                queryParam(ORDERID_NAME, orderID).
-                queryParam(PAYINFO_NAME, payInfoValue).
-        when().
+                queryParam(ORDER_ID_NAME, orderID).
+                queryParam(PAY_INFO_NAME, payInfoValue).
+                when().
                 get(BASE_URL).
-        then().
+                then().
                 log().all().
                 assertThat()
                 .statusCode(200)
@@ -53,6 +64,127 @@ public class PaytureAPIBlockRequestTests {
                 .body(containsString("OrderId=\"" + orderID + "\""))
                 .and()
                 .body(containsString("Amount=\"" + AMOUNT_VALUE + "\""));
+
+    }
+
+    @Test
+    @Description("Отправить GET запрос с корректно заполненными обязательными полями (OrderID, Amount, PayInfo) и без параметра Key")
+    public void test_sendRequestWithoutKey() {
+        given().
+                queryParam(AMOUNT_NAME, AMOUNT_VALUE).
+                queryParam(ORDER_ID_NAME, orderID).
+                queryParam(PAY_INFO_NAME, payInfoValue).
+                when().
+                get(BASE_URL).
+                then().
+                log().all().
+                assertThat()
+                .statusCode(200)
+                .and()
+                .body(containsString("Block"))
+                .and()
+                .body(containsString("Success=\"False\""))
+//                .and()
+//                .body(containsString("OrderId=\"" + orderID + "\""))
+                .and()
+                .body(containsString(ERROR_CODE_ACCESS_DENIED));
+
+    }
+
+    @Test
+    @Description("Отправить GET запрос с корректно заполненными обязательными полями (Key, OrderId, Amount, PayInfo). В значении OrderId передать ранее использованный номер заказа")
+    public void test_sendRequestWithDuplicateOrderID() {
+        given().
+                queryParam(KEY_NAME, KEY_VALUE).
+                queryParam(AMOUNT_NAME, AMOUNT_VALUE).
+                queryParam(ORDER_ID_NAME, duplicateOrderId).
+                queryParam(PAY_INFO_NAME, payInfoValue).
+                when().
+                get(BASE_URL).
+                then().
+                log().all().
+                assertThat()
+                .statusCode(200)
+                .and()
+                .body(containsString("Block"))
+                .and()
+                .body(containsString("Success=\"False\""))
+                .and()
+                .body(containsString("OrderId=\"" + duplicateOrderId + "\""))
+                .and()
+                .body(containsString(ERROR_CODE_DUPLICATE_ORDER_ID));
+
+    }
+
+    @Test
+    @Description("Отправить GET запрос с корректно заполненными обязательными полями (Key, Amount, PayInfo) и без параметра OrderId")
+    public void test_sendRequestWithoutOrderId() {
+        given().
+                queryParam(KEY_NAME, KEY_VALUE).
+                queryParam(AMOUNT_NAME, AMOUNT_VALUE).
+                queryParam(PAY_INFO_NAME, payInfoValue).
+                when().
+                get(BASE_URL).
+                then().
+                log().all().
+                assertThat()
+                .statusCode(200)
+                .and()
+                .body(containsString("Block"))
+                .and()
+                .body(containsString("Success=\"False\""))
+                .and()
+                .body(containsString("OrderId=\"\""))
+                .and()
+                .body(containsString(ERROR_CODE_WRONG_PARAMS));
+
+    }
+
+    @Test
+    @Description("Отправить GET запрос с корректно заполненными обязательными полями (Key, OrderId, PayInfo) и без параметра Amount")
+    public void test_sendRequestWithoutAmount() {
+        given().
+                queryParam(KEY_NAME, KEY_VALUE).
+                queryParam(ORDER_ID_NAME, orderID).
+                queryParam(PAY_INFO_NAME, payInfoValue).
+                when().
+                get(BASE_URL).
+                then().
+                log().all().
+                assertThat()
+                .statusCode(200)
+                .and()
+                .body(containsString("Block"))
+                .and()
+                .body(containsString("Success=\"False\""))
+                .and()
+                .body(containsString("OrderId=\"" + orderID + "\""))
+                .and()
+                .body(containsString(ERROR_CODE_AMOUNT_ERROR));
+
+    }
+
+    @Test
+    @Description("Отправить GET запрос с корректно заполненными обязательными полями (Key, Amount, OrderId) и без параметра PayInfo")
+    public void test_sendRequestWithoutPayInfo() {
+        given().
+                queryParam(KEY_NAME, KEY_VALUE).
+                queryParam(AMOUNT_NAME, AMOUNT_VALUE).
+                queryParam(ORDER_ID_NAME, orderID).
+                when().
+                get(BASE_URL).
+                then().
+                log().all().
+                assertThat()
+                .statusCode(200)
+                .and()
+                .body(containsString("Block"))
+                .and()
+                .body(containsString("Success=\"False\""))
+//                .and()
+//                .body(containsString("OrderId=\"" + orderID + "\""))
+                .and()
+                .body(containsString(ERROR_CODE_WRONG_PAY_INFO));
 
     }
 }
